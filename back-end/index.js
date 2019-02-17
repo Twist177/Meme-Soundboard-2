0f1b881d87a117846a5a59424ad193c090fa74d0
Ferris Esposito <ferrisesposito@gmail.com>
2019-02-17 00:52:43 -0500

    Fix merge conflict

--- back-end/index.js
+++ back-end/index.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const mysql = require('mysql');
-var app = express();
-
-app.get('/', function (req, res) {
-	//res.write(path.join(__dirname + '/../header.html'));
-	//res.write(path.join(__dirname + '/../home.html'));
-
-	var con = mysql.createConnection({
-	  host     : '35.232.110.39',
-	  user     : 'root',
-	  password : 'meme',
-	  database : 'memes'
-	});
-	con.connect(function(err) {
-		if (err) {
-			console.error('error connecting: ' + err.stack);
-			return;
-		}
-		console.log('connect as id ' + con.threadId);
-	});
-	con.query('SELECT * FROM users', function (error, results, fields) {
-  		if (error) throw error;
-  		console.log('The solution is: ', results[0].userName);
-	});
-	con.end();
-
-	res.write(fs.readFileSync(__dirname + '/../header.html', 'utf8'));
-	res.write(fs.readFileSync(__dirname + '/../home.html', 'utf8'));
-	res.end();
-})
-
-var server = app.listen(process.env.PORT || 80, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at http://%s:%s", host, port)
-=======
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -84,5 +43,4 @@ var server = app.listen(process.env.PORT || 80, function () {
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
->>>>>>> 143276fce5fefb78f347cf9c56ee30b1723d2abc
 })
